d770e278b297eb035f44fca9e965de805b40ac1f
Krishi Agrawal <krishi.agrawal26@gmail.com>
2024-05-02 08:06:21 +0530

    more access header added

--- server/server.js
+++ server/server.js
@@ -16,6 +16,13 @@ app.use(cors({
     methods: ['POST', 'GET',  'PUT', 'DELETE'],
     credentials: true
 }))
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization")
+      next();
+    });
 app.use(bodyParser.json({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: false }))
 
